718762a4fe038da7039a86137f38fcc05eaff4b9
shri-rang <jangamshrirang003@gmail.com>
2024-06-15 18:33:42 +0530

    sorting added

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,12 +1,8 @@
 const Product = require('../models/product')
 
 const getAllproductsStatic = async(req,res)=>{
- 
-      const products = await Product.find({
-        //  featured: true,
-        name: 'vase table'
-         
-      });
+      const search = 'ab';
+      const products = await Product.find({ }).sort('-name price');
   //  throw new Error("testing aysnc error")
 
    res.status(200).json({products, nbHits:products.length })
@@ -15,16 +11,41 @@ const getAllproductsStatic = async(req,res)=>{
 
 const getAllproducts = async(req,res)=>{
       console.log(req.query);
-      const { featured} = req.query
+      const { featured, company, name, sort,fields} = req.query
        const queryObject = {}
 
         if(featured){
         queryObject.featured = featured === 'true' ? true : false 
         }
+        if(company){
+         queryObject.company =  company
+        }
 
-       const products = await Product.find(queryObject);
+        if(name){
+            queryObject.name = {$regex:name,$options:'i'}
+        }
+
+      
+        
+       let result =  Product.find(queryObject)
+       //sort
+        if(sort){
+       //  products = products.sort();
+         const sortList = sort.split(',').join(' ');
+         console.log(sortList);
+         result = await result.sort(sortList);
+        }else{
+          result = await result.sort('createdAt');   
+        }
+
+         if(fields){
+          const fieldsList = fields.split(',').join(' ');
+         console.log(fieldsList);
+         result = await result.select(fieldsList);
+         }
+        const products = await result;
     // throw   Error("testing aysnc error");
- res.status(200).json({products, nbHits:products.length})
+       res.status(200).json({products, nbHits:products.length})
 }
 
 
